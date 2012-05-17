4435cc50842b277446bb9622d842edc7968ac1b3
Adrian Banks <github@adrianbanks.co.uk>
2012-05-18 00:09:30 +0100

    make the solutions compile correctly
    - add a reference to NUnit for the test project
    - compile to bin, not bin/debug and bin/release
    - add an 'Any CPU' compilation

--- SqlDiff/AssemblyInfo.cs
+++ SqlDiff/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SqlDiff")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("801650f1-81e5-47dc-8bd9-804085bdcc91")]
+
+[assembly: InternalsVisibleTo("SqlDiffTests")]
