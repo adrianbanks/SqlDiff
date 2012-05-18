fe3efefd293015c61e98c38af1f9a3fe7ef1bdb0
Adrian Banks <github@adrianbanks.co.uk>
2012-05-18 21:19:51 +0100

    Add the basis for the schema comparer

---
+++ SqlDiff/SchemaComparer.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using AdrianBanks.SqlDiff.Comparison;
+using AdrianBanks.SqlDiff.SchemaItems;
+
+namespace AdrianBanks.SqlDiff
+{
+    internal sealed class SchemaComparer
+    {
+        private readonly Schema left;
+        private readonly Schema right;
+
+        public SchemaComparer(Schema left, Schema right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public Report Compare()
+        {
+            var tables = CompareTables();
+            var views = CompareViews();
+            var triggers = CompareTriggers();
+
+            var comparison = tables.Concat(views).Concat(triggers);
+            return new Report(comparison);
+        }
+
+        private IEnumerable<ComparisonObject> CompareTables()
+        {
+            var comparison = new List<ComparisonObject>();
+            return comparison;
+        }
+
+        private IEnumerable<ComparisonObject> CompareViews()
+        {
+            var comparison = new List<ComparisonObject>();
+            return comparison;
+        }
+
+        private IEnumerable<ComparisonObject> CompareTriggers()
+        {
+            var comparison = new List<ComparisonObject>();
+            return comparison;
+        }
+    }
+}
